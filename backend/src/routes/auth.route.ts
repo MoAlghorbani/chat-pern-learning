8da90c3ceb96bbedecf05e83a129bca58f62328b
Mohammed Fadl <mohammadefathl@gmail.com>
2025-04-10 01:45:33 +0300

    inti project

---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// import { login, logout, signup, getMe } from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/me", protectRoute as any, getMe as any);
+
+router.post("/signup", signup as any);
+router.post("/login", login as any);
+router.post("/logout", logout);
+
+export default router;
